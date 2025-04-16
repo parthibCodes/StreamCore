14d5ffa27ef16394adcb9e262017ce96acb7be23
Parthib <parthib.chakraborty.dev@gmail.com>
2025-04-16 10:50:08 +0530

    utils code making

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./db/database.js";
+import "./db/database.js";
 import express from "express";
+import { app } from "./app.js";
 
-connectDB()
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT , ()=>{
+    console.log(`Server is listening in port ${PORT}`);
+});
