f93de1168850a65833e2671ad8fed5958dffd87e
Parthib <parthib.chakraborty.dev@gmail.com>
2025-04-17 10:34:46 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-import "./db/database.js";
-import express from "express";
+import connectDB from "./db/database.js";
 import { app } from "./app.js";
-
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT , ()=>{
-    console.log(`Server is listening in port ${PORT}`);
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is listening at port ${PORT}`);
+    });
+}).catch((err)=>{
+    console.error("Failed to start server due to DB connection error:", err);
 });
