004cc1ee5d7dd31b21e03434d415ec6e163c1ce1
Parthib <parthib.chakraborty.dev@gmail.com>
2025-04-22 09:18:18 +0530

    Create register user cotroller and routes and some bug fixes

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,44 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
+import { uploadOnCLoudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async(req,res,next)=>{
+    const {username,email,fullname,paassword} = req.body;
+    if([fullname,username,email,paassword].some((field)=>
+    field?.trim() === "")
+    ){
+        throw new ApiError(400,"All fields are required");
+    }
+    const existedUser = User.findOne({$or:[{username},{email}]})
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exists");
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required");
+    }
+    const avatar = await uploadOnCLoudinary(avatarLocalPath)
+    const coverImage = await uploadOnCLoudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar file is required");
+    }
+    const user = await User.create({
+        fullname,avatar:avatar.url,coverImage:coverImage?.url || "",email,paassword,username:username.toLowerCase()
+    });
+    const createdUser = await User.findById(user.id).select("-password -refreshToken");
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
+})
+
+
+export {registerUser};
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
 const asyncHandler= (requestHandler) =>{
-    (req,res,next)=>{Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))}
+    return (req,res,next)=>{Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))}
 }
 export {asyncHandler};
